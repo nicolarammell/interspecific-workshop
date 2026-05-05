d07cec21dabaa21cdee53937287c4c8d2dc86993
nicolarammell <nicolarammell@gmail.com>
2026-05-05 15:59:11 -0400

    edits

--- code/map.R
+++ code/map.R
@@ -30,8 +30,19 @@ sites <- readr::read_csv(here::here("data/raw/dendro_site.csv"))
 View(sites)
 
 # read in infectection data from M. Mihorean
-infection <- readr::read_csv(here::here("data/raw/Nicolaaspen.csv")) 
+infection <- readr::read_csv(here::here("data/raw/Nicolaaspen.csv")) %>%
+  group_by(siteID) %>% tally(margin_tree) %>%
+  drop_na(n) %>%
+  dplyr::filter(!siteID == "NEAR 80F24") %>%
+  mutate(n = as.character(n)) %>%
+  mutate(site_infection = if_else(n == "0", "No", "Yes")) %>%
+  dplyr::rename(num_margin_trees = n) %>%
+  dplyr::rename(site = siteID)
 View(infection)
+
+# join df together
+sites_infection <- left_join(infection, sites, by = "site")
+View(sites_infection)
 
 # read in fire perimeter shape files (hand-drawn) - provided by Brian Newton (60s, 70s, 80s, 90s, 00s)
 perimeters_bn <- st_read("data/raw/fire_perimeters/Rebuilt_fixed_fire_perimeters.shp") %>%
@@ -60,18 +71,20 @@ circle_colours <- c("lightgreen","lightgreen","darkgreen","orange")
 palette_sites <- colorFactor(palette = circle_colours, domain = sites$spp_samples)
 
 #creating the map
-   sites %>%
+   sites_infection %>%
   dplyr::filter(str_detect(spp_sampled, ('pop_tr'))) %>%
   leaflet() %>%
   addTiles() %>%
-  addProviderTiles("Esri.WorldImagery") %>% 
+  addProviderTiles("Esri.WorldTopoMap") %>% # Esri.WorldTopoMap; # Esri.WorldImagery; #Esri.WorldStreetmap
   addPolygons(data = perimeters_all,
               fillColor = ~palette(DECADE), 
               color = "black", 
-              fillOpacity = 0.9) %>%
+              fillOpacity = 0.5, 
+              weight = 1) %>%
   addCircleMarkers(lng = ~x, lat = ~y, 
                    color = ~palette_sites(spp_sampled), 
-                   label = ~site) %>% 
+                   label = ~site, 
+                   ~ifelse(site_infection == "Yes", 5, 1),) %>% 
   addLegend(data = perimeters_all, 
             pal = palette, 
             values = ~DECADE, 
