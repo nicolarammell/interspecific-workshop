90ca7dbd9ff62bedc0b78eced8a5de90238f361e
Branavi Kumar <branavikumar@Branavis-MacBook-Pro.local>
2026-05-05 13:42:39 -0400

    map

---
+++ code/map.R
@@ -0,0 +1,24 @@
+## Script: map.R
+## Interspecific Workshop
+## Authors: NR & BK (2026)
+## Forest Ecology Research Lab
+## Wilfrid Laurier University
+
+# This script is used to visualize sites in terms of their location in the Yukon
+
+# LOAD LIBRARIES ----
+
+# load libraries
+library(here)      # for relative working directory 
+library(tidyverse) # dplyr, forcats, gglot2, lubridate, purrr, readr, stringr, tibble, tidyr
+library(leaflet)   # mapping sites
+library(sf)        # mapping sites
+library(ggridges)  # for making interspecific plot
+library(dplR)      # dendro
+library(treeclim)  # dendro
+library(TRADER)    # dendro
+library(graphics)  # dendro
+library(utils)     # dendro
+
+# set theme
+theme_set(theme_classic())
